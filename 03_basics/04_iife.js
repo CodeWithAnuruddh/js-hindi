b2c5f486dc1005349925cc028139f57b8366de3a
CodeWithAnuruddh <154063415+CodeWithAnuruddh@users.noreply.github.com>
2024-01-07 06:10:16 +0000

    function commit

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,18 @@
+//immidiately invoked function expressions(IIFE)
+
+function two(){
+    console.log(`DB connected`);
+}
+two();
+
+//now how to use iife syntax ()();
+
+(function two(){
+    console.log(`DB connected`);
+})  ();  //last semicolon lagana jaruri hai nahi to error ayega
+
+// now one more type by arrow method
+
+( (name) => {
+    console.log(`DB connected Two ${name}`);
+} ) ('hitesh');
