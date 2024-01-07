5c5909c0f6392560f7747324ef82bc6d2a5f7320
CodeWithAnuruddh <154063415+CodeWithAnuruddh@users.noreply.github.com>
2024-01-07 07:09:44 +0000

    nothing

---
+++ 03_basics/05_javascriptExecutiionContext.js
@@ -0,0 +1,14 @@
+//javascript execution context
+
+// 1- Global execution context
+// browser ke andar `this` ki value window object aati hai
+// it execute in thread and javascrpt is single threaded
+
+// 2-functional execution context 
+// 3-eval execution context 
+
+
+// full process is in the screenshots of this pc
+
+
+//call
